929fdfb018bbb04e0b0c3d1941d4f81537adf9c9
Max Ball <max.ball3388@gmail.com>
2022-12-05 12:17:11 -0700

    removed token

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,6 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  // remove token and change back to null
-  // token: 'BQDqGnccsbaNpR1tX9jyH_MnZHZZXhbdxtpTL7buyyyXH42hzGUyIqOWUgeHg9vbrpdNxLpzXAKp_cZHvhF67AVevxrNqDPOWu2qlqhIgwnqX8ZOPs-Omr41a3lPIK17sfV1ZmzQkVBY0h_w3T3gNkOq1zD-Pc_qnwuEcMAuif5x8tU2VrIZ05gSCLt8BoewrFQlYb2iMF4yKETF5JWb',
 }
 
 const reducer = (state, action) => {
